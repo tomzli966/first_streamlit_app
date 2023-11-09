127607e77fc3d05dd6bdaac322e0b3576d4cf869
tomzli966 <71530651+tomzli966@users.noreply.github.com>
2023-11-09 01:32:19 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('Breakfast Menu')
+treamlit.title('Omega 3 & Blueberry Oatmeal')
+treamlit.title('Kale, Spinach & Rocket Smoothie')
+treamlit.title('Hard-Boiled Free-Range Egg')
